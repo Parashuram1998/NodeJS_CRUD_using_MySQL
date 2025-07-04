03eaa957bebb5273e73fcb9240f00a59695ea5dc
Parasharam Pujari <pujarip516@gmail.com>
2025-07-04 16:22:14 +0530

    Created the application Node JS CRUD using MySQL

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const mysqlPool = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "Mysql!2025",
-  database: "employee_db",
+  user: process.env.USER,
+  password: process.env.password,
+  database: process.env.Database,
 });
 
 module.exports = mysqlPool;
