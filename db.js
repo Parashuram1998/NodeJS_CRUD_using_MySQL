00ffaf0eb94e94ade8a31956bead1c6b8f90b6cf
Parasharam Pujari <pujarip516@gmail.com>
2025-08-25 11:43:59 +0530

    Updated db.js file

--- db.js
+++ db.js
@@ -1,12 +1,30 @@
-const mysql = require("mysql2/promise");
-const dotenv = require("dotenv");
-dotenv.config();
+// const mysql = require("mysql2/promise");
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-const mysqlPool = mysql.createPool({
-  host: process.env.Host,
-  user: process.env.USER,
-  password: process.env.password,
-  database: process.env.Database,
-});
+// const mysqlPool = mysql.createPool({
+//   host: process.env.Host,
+//   user: process.env.user,
+//   password: process.env.Password,
+//   database: process.env.Database,
+// });
 
-module.exports = mysqlPool;
+// module.exports = mysqlPool;
+
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const pool = mysql
+  .createPool({
+    host: process.env.Host,
+    user: process.env.user,
+    password: process.env.Password,
+    database: process.env.Database,
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  })
+  .promise();
+
+module.exports = pool;
